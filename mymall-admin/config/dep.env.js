8300fe2b322067b5fd3a04174dbf5c7570f62e9d
liuyanlin <859071935@qq.com>
2020-04-11 16:50:20 +0800

    Initial commit

---
+++ mymall-admin/config/dep.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+	NODE_ENV: '"production"',
+  ENV_CONFIG: '"dep"',
+  BASE_API: '"http://106.15.250.22:8080/admin"'
+}
---
+++ mymall-wx/utils/user.js
@@ -0,0 +1,97 @@
+/**
+ * 用户相关服务
+ */
+const util = require('../utils/util.js');
+const api = require('../config/api.js');
+
+
+/**
+ * Promise封装wx.checkSession
+ */
+function checkSession() {
+  return new Promise(function(resolve, reject) {
+    wx.checkSession({
+      success: function() {
+        resolve(true);
+      },
+      fail: function() {
+        reject(false);
+      }
+    })
+  });
+}
+
+/**
+ * Promise封装wx.login
+ */
+function login() {
+  return new Promise(function(resolve, reject) {
+    wx.login({
+      success: function(res) {
+        //res.code 用户登陆凭证（有效期五分钟）开发者需要在开发者服务器后台调用 api，使用 code 换取 openid 和 session_key 等信息
+        if (res.code) {
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      },
+      fail: function(err) {
+        reject(err);
+      }
+    });
+  });
+}
+
+/**
+ * 调用微信登录
+ */
+function loginByWeixin(userInfo) {
+
+  return new Promise(function(resolve, reject) {
+    //这个res 其实是code
+    return login().then((res) => {
+      //登录远程服务器
+      //res.code 用户登陆凭证（有效期五分钟）开发者需要在开发者服务器后台调用 api，使用 code 换取 openid 和 session_key 等信息
+      //util.request 封装了微信的request
+      util.request(api.AuthLoginByWeixin, {
+        code: res.code,
+        userInfo: userInfo
+      }, 'POST').then(res => {
+        if (res.errno === 0) {
+          //存储用户信息
+          wx.setStorageSync('userInfo', res.data.userInfo);
+          wx.setStorageSync('token', res.data.token);
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+    }).catch((err) => {
+      reject(err);
+    })
+  });
+}
+
+/**
+ * 判断用户是否登录
+ */
+function checkLogin() {
+  return new Promise(function(resolve, reject) {
+    if (wx.getStorageSync('userInfo') && wx.getStorageSync('token')) {
+      checkSession().then(() => {
+        resolve(true);
+      }).catch(() => {
+        reject(false);
+      });
+    } else {
+      reject(false);
+    }
+  });
+}
+
+module.exports = {
+  loginByWeixin,
+  checkLogin,
+};
